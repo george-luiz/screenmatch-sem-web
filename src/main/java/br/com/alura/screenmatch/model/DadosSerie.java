377fab2f19e99a8638c5432852f5475691ab4c07
george-luiz <geluiz275@gmail.com>
2024-06-16 15:49:45 -0300

    Adicionando projeto.

---
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,12 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true) //JsonIgnoreProperties: ignora o que vem da API que não estiver mapeado nesta classe
+public record DadosSerie (
+        @JsonAlias("Title") String titulo, //JsonAlias irá buscar na API exatamente o Title e irá incluir no titulo
+        @JsonAlias("totalSeasons") Integer totalTemporadas,
+        @JsonAlias("imdRating") String avaliacao) {
+
+}
